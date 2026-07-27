95021f9d964f214748925a35f1c8bfba17a28e52
rohit mathur <mathurrohit1674@gmail.com>
2026-07-27 21:25:44 +0530

    feat: add authentication and user-specific trip management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
-import { Route, Routes } from "react-router";
-
+import { Navigate, Route, Routes } from "react-router";
+import RegisterPage from "./pages/RegisterPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import CreateTripPage from "./pages/CreateTripPage";
 import ProcessingPage from "./pages/ProcessingPage";
@@ -9,11 +11,17 @@ import HistoryPage from "./pages/HistoryPage";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/create-trip" element={<CreateTripPage />} />
-      <Route path="/processing" element={<ProcessingPage />} />
-      <Route path="/result" element={<ResultPage />} />
-      <Route path="/history" element={<HistoryPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create-trip" element={<CreateTripPage />} />
+        <Route path="/processing" element={<ProcessingPage />} />
+        <Route path="/result" element={<ResultPage />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Route>
+
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
